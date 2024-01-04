b0dc87411c3d74e7cf97e5b0925cd9700ccde7fe
EikkoMass <ericmassaneiro@gmail.com>
2024-01-04 20:44:33 -0300

    adicionando form para criacao de noticias + body-parser

--- config/server.js
+++ config/server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 let consign = require('consign');
+let bodyParser = require('body-parser');
+
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 consign()
   .include('app/routes')
