70b7bbd5857fdc09f22c6f4ce6e0acc1e1a4e83e
EikkoMass <ericmassaneiro@gmail.com>
2024-01-07 20:42:15 -0300

    adicionando express-validator como middleware

--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
-var express = require('express');
+let express = require('express');
 let consign = require('consign');
 let bodyParser = require('body-parser');
+let expressValidator = require('express-validator');
 
 
 var app = express();
@@ -8,6 +9,7 @@ app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressValidator());
 
 consign()
   .include('app/routes')
