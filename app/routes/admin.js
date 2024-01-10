c128681bd8d04c021dd687d03d2a5f8e829009d3
EikkoMass <ericmassaneiro@gmail.com>
2024-01-10 00:55:53 -0300

    validacao de campos por middleware e adicionando mais campos para noticias

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -7,6 +7,20 @@ module.exports = function (application) {
     application.post('/noticias/salvar', (req, res) => {
         let noticia = req.body;
 
+        req.assert('titulo', 'Titulo e obrigatorio').notEmpty();
+        req.assert('resumo', 'Resumo e obrigatorio').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('autor', 'Autor e obrigatorio').notEmpty();
+        req.assert('data_noticia', 'Data e obrigatoria').notEmpty().isDate({ format: 'YYYY-MM-DD' });
+        req.assert('noticia', 'Noticia e obrigatoria').notEmpty();
+
+        let erros = req.validationErrors();
+
+        if (erros) {
+            res.render('admin/form_add_noticia');
+            return;
+        }
+
         let connection = application.config.dbConnection;
         let noticiasModel = new application.app.models.NoticiasDAO(connection);
 
