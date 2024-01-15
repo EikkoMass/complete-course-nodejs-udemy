6f258f68036d467c855e52299b0f19a0c5bf9025
EikkoMass <ericmassaneiro@gmail.com>
2024-01-15 16:27:06 -0300

    adicionando valores dinamicamente no painel principal

--- app/models/NoticiasDAO.js
+++ app/models/NoticiasDAO.js
@@ -16,4 +16,8 @@ NoticiasDAO.prototype.salvarNoticia = function (noticia, callback) {
   this._connection.query(`INSERT INTO noticias set ?`, noticia, callback);
 }
 
+NoticiasDAO.prototype.get5UltimasNoticias = function (callback) {
+  this._connection.query(`SELECT * FROM noticias ORDER BY data_criacao DESC LIMIT 5`, callback);
+}
+
 module.exports = () => NoticiasDAO
