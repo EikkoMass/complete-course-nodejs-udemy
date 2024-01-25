b464e3537e50f2958820d7692468c9beadb1bbb9
EikkoMass <ericmassaneiro@gmail.com>
2024-01-24 23:47:56 -0300

    adicionando validacao de campos para tela de acesso

--- multiroom_chat/app/controllers/chat.js
+++ multiroom_chat/app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.iniciaChat = function (application, req, res) {
+  let dadosForm = req.body;
+
+  req.assert('apelido', 'Nome ou Apelido e obrigatorio').notEmpty();
+  req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+
+  let validacao = req.validationErrors();
+
+  if (validacao) {
+    res.render('index', { validacao });
+    return;
+  }
+
   res.render('chat');
 }
