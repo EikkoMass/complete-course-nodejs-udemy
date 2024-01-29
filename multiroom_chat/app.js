1958a4c620922674fe58ce7a44198c82af437a06
EikkoMass <ericmassaneiro@gmail.com>
2024-01-29 18:31:39 -0300

    recebendo mensagens enviadas em tempo real em ambos os lados

--- multiroom_chat/app.js
+++ multiroom_chat/app.js
@@ -11,10 +11,18 @@ let io = require('socket.io').listen(server);
 app.set('io', io);
 
 /* Criar a conexao por websocket */
-io.on('connection', (socket) => {
+io.on('connection', socket => {
   console.log('usuario conectou');
 
   socket.on('disconnect', () => {
     console.log('usuario desconectou');
+  });
+
+  socket.on('msgParaServidor', data => {
+    /* retorna para o usuario que requisitou o socket*/
+    socket.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem });
+    
+    /* retorna para todos os usuarios que estao conectados no socket (exceto o que requisitou)*/
+    socket.broadcast.emit('msgParaCliente', {apelido: data.apelido, mensagem: data.mensagem });
   });
 });
