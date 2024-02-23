6d9407e6d8c2951e73d494f291f8ed9a70a16b05
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-02-23 15:48:42 -0300

    introducao a sessions e validando campos de autenticacao

--- 4_mmorpg_got/app/controllers/index.js
+++ 4_mmorpg_got/app/controllers/index.js
@@ -1,3 +1,20 @@
-module.exports.index = function (application, req, res) {
-  res.render('index')
+module.exports = {
+  index: function (application, req, res) {
+    res.render('index', {validacao: {}})
+  },
+  autenticar: function (application, req, res) {
+    
+    let dadosForm = req.body;
+    
+    req.assert('usuario', 'Usuario nao deve ser vazio').notEmpty();
+    req.assert('senha', 'Senha nao deve ser vazia').notEmpty();
+
+    let erros = req.validationErrors();
+  
+    if(erros) {
+      res.render('index', {validacao: erros})
+    }
+  
+    res.send('certo');
+  }
 }
