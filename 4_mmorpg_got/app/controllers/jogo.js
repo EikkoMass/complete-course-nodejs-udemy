69e7260323e87bb17c779426dbd1ded8c8020551
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-02-28 00:47:27 -0300

    destruindo sessao de usuario logado

--- 4_mmorpg_got/app/controllers/jogo.js
+++ 4_mmorpg_got/app/controllers/jogo.js
@@ -1,9 +1,17 @@
-module.exports.jogo = function (application, req, res) {
-  
-  if(req.session.autorizado)
-  {
-    res.render('jogo');
-  }
+module.exports = {
 
-  res.send('Usuario precisa fazer o login');
+  jogo: function (application, req, res) {
+
+    if (req.session.autorizado) {
+      res.render('jogo');
+    } else {
+      res.send('Usuario precisa fazer o login');
+    }
+  },
+
+  sair: function (application, req, res) {
+    req.session.destroy(function(err) {
+      res.render('index',{validacao: {}});
+    });
+  }
 }
