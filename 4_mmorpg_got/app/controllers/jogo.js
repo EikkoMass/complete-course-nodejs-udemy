ebd8092af1ec45bcb2bf64d88a8a847dabca5192
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2024-02-29 18:30:22 -0300

    adicionando dinamicamente casa de usuario

--- 4_mmorpg_got/app/controllers/jogo.js
+++ 4_mmorpg_got/app/controllers/jogo.js
@@ -3,7 +3,7 @@ module.exports = {
   jogo: function (application, req, res) {
 
     if (req.session.autorizado) {
-      res.render('jogo');
+      res.render('jogo', {img_casa: req.session.casa});
     } else {
       res.send('Usuario precisa fazer o login');
     }
